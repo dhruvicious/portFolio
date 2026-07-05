61e4b8939d9d925da68fa23491a173bc223e5730
Dhruv Khatri <dhruvkhatri1234@gmail.com>
2026-07-05 14:19:03 +0530

    fix

--- components/work-section/index.tsx
+++ components/work-section/index.tsx
@@ -4,12 +4,7 @@ import styles from "./work-section.module.css";
 
 export function WorkSection() {
   return (
-    <section id="my-work" className={styles.scrollContainer} data-nav-theme="hidden">
-      {/* The navbar checks the bottom of the screen. 
-          By placing this trigger 100vh down, the navbar turns 'light' 
-          exactly when the top of the My Work section hits the top of the screen! */}
-      <div style={{ position: "absolute", top: "100vh", bottom: 0, width: "100%", pointerEvents: "none" }} data-nav-theme="light" />
-
+    <section id="my-work" className={styles.scrollContainer} data-nav-theme="light">
       <div className={styles.stickySection}>
         <h2 className={styles.sectionHeading}>My Work</h2>
       </div>
